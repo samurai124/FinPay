381b22a4e77a06807b5bf0185ab8ef82a6cc1532
Hamza <hamzazaidi253@gamil.com>
2026-02-09 11:32:04 +0100

    validing beed added to the app

--- src/main/java/util/ValidationDonnees.java
+++ src/main/java/util/ValidationDonnees.java
@@ -1,5 +1,47 @@
 package util;
 
-public class ValidationDonnees {
+import java.util.Scanner;
 
+public class ValidationDonnees {
+    public static Scanner input = new Scanner(System.in);
+
+    public static int validateInts(String fieldName) {
+        int validatedInt = 0;
+        do {
+            System.out.print("Saisir " + fieldName + " : ");
+            try {
+                validatedInt = Integer.parseInt(input.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Veuillez saisir un(e) " + fieldName + " valide.");
+                validatedInt = 0;
+            }
+        } while (validatedInt == 0);
+        return validatedInt;
+    }
+
+    public static float validateFloats(String fieldName) {
+        float validatedFloat = 0;
+        do {
+            System.out.print("Saisir " + fieldName + " : ");
+            try {
+                validatedFloat = Float.parseFloat(input.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Veuillez saisir un(e) " + fieldName + " valide.");
+                validatedFloat = 0;
+            }
+        } while (validatedFloat == 0);
+        return validatedFloat;
+    }
+
+    public static String validateString(String fieldName) {
+        String validatedString = "";
+        do {
+            System.out.print("Saisir " + fieldName + " : ");
+            validatedString = input.nextLine();
+            if (validatedString.isEmpty()) {
+                System.out.println("Veuillez saisir un(e) " + fieldName + " valide.");
+            }
+        } while (validatedString.isEmpty());
+        return validatedString;
+    }
 }
