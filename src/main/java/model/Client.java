d2655bd2feb1a505082913d35fc238183e6c0fba
loubnaaktar <loubnaakt13@gmail.com>
2026-02-12 12:25:31 +0100

    les fonction du client

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -4,6 +4,10 @@ public class Client {
     private int id;
     private String nom;
 
+    public Client(String nom){
+        this.nom = nom;
+    }
+
     public Client(int id, String nom) {
         this.id = id;
         this.nom = nom;
