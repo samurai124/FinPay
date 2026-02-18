1a01270cb590aa8d1b669a77e7d56f54c40ff00c
Imane Ramadane <ramadaneimane@gmail.com>
2026-02-18 12:03:22 +0000

    ajout de fileOutput Stream

--- src/main/java/Excel/ExcelPrestatire.java
+++ src/main/java/Excel/ExcelPrestatire.java
@@ -9,6 +9,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import util.DBconnection;
 import util.ValidationDonnees;
 
+import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,7 +18,7 @@ import java.util.List;
 import static util.DBconnection.getConnection;
 
 public class ExcelPrestatire {
-    public void afficherFacturePrestatire(){
+    public  static void afficherFacturePrestatire(){
         int id= ValidationDonnees.validateInts("id");
 
         List<Facture> factures=dao.FactureDAO.getFacturesByPrestataire(id);
@@ -36,13 +37,15 @@ public class ExcelPrestatire {
     header.createCell(2).setCellValue("Client");
     header.createCell(3).setCellValue("Montant");
     header.createCell(4).setCellValue("Status");
+    //création de header:
+
         for(Facture f:factures){
             Row row = sheet.createRow(rowN++);
             row.createCell(0).setCellValue(f.getId());
-//            row.createCell(1).setCellValue(f.getDate());
+            row.createCell(1).setCellValue(f.getDate());
             row.createCell(2).setCellValue(f.getClient().getNom());
             row.createCell(3).setCellValue(f.getMontant());
-            row.createCell(4).setCellValue(f.getStatut());
+            row.createCell(4).setCellValue(f.getStatut()?"payée":"non payée");
         }
 
 
@@ -51,17 +54,37 @@ public class ExcelPrestatire {
 
     //calcul facturé
    totalfacture = factures.stream().mapToDouble(f->f.getMontant()).sum();
+        Row totalFactureRow=sheet.createRow(rowN++);
+        totalFactureRow.createCell(2).setCellValue("calcul facturé");
+        totalFactureRow.createCell(3).setCellValue(totalfacture);
 
 
 
    //Total payé
   totalpaye = factures.stream().filter(f->f.getStatut()).mapToDouble(f->f.getMontant()).sum();
+  Row totalPayeRow=sheet.createRow(rowN++);
+  totalPayeRow.createCell(2).setCellValue("total payé");
+  totalPayeRow.createCell(3).setCellValue(totalpaye);
 
     //Total non payé
         totalNonPaye = factures.stream().filter(f->!f.getStatut()).mapToDouble(f->f.getMontant()).sum();
+        Row totalNonPayeRow=sheet.createRow(rowN++);
+        totalNonPayeRow.createCell(2).setCellValue("total non payé");
+        totalNonPayeRow.createCell(3).setCellValue(totalNonPaye);
 
 
 
+  try{
+      FileOutputStream file= new FileOutputStream(" facturesprestatairemois.xls");
+      workbook.write(file);
+      file.close();
+      workbook.close();
+      System.out.println("Export Excel réussi");
+
+  }catch(Exception e){
+      e.printStackTrace();
+  }
     }
+
 
 }
