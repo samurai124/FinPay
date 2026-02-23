bca21308ce382799cc4ef6b735fc711f7a5a7ab2
Imane Ramadane <ramadaneimane@gmail.com>
2026-02-23 15:17:28 +0000

    test des montants

--- src/test/java/com/example/CommissionTest.java
+++ src/test/java/com/example/CommissionTest.java
@@ -1,4 +1,50 @@
 package com.example;
+import org.junit.jupiter.api.*;
+import service.PaimentService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 
 public class CommissionTest {
-}
+    private PaimentService paimentservice;
+    @BeforeEach
+    void setUp(){
+        paimentservice = new PaimentService();
+
+    }
+    @Test
+    @DisplayName("Test 1: montant normal")
+    void MontantNormal(){
+          double res= paimentservice.calculerCommistion(1000);
+          assertEquals(20,res);
+
+
+    }
+    @Test
+    @DisplayName("Test 2:montant égale à zéro")
+        void MontantZero() {
+            double res = paimentservice.calculerCommistion(0.0);
+            assertEquals(0.0, res);
+        }
+
+
+
+
+    @Test
+    @DisplayName("Test 3:Montant élevé")
+    void MontantEleve(){
+        double res=paimentservice.calculerCommistion( 1000000);
+        assertEquals(20000.0,res);
+
+    }
+
+
+
+    }
+
+
+
+
+
+
